e98a6c019a4bc248f436ae0ae53d34dff29159b5
taewan36 <92144840+taewan36@users.noreply.github.com>
2022-02-07 17:29:16 +0900

    Initial commit

---
+++ src/main/java/com/icia/tw/TwApplication.java
@@ -0,0 +1,13 @@
+package com.icia.tw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/icia/tw/TwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icia.tw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
